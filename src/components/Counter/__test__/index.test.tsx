c9d12e6076b212a9c724ec046dca9e6bb28e2b5f
mpsalunggg <putrasatria893@gmail.com>
2024-06-13 23:37:00 +0800

    add describe

--- src/components/Counter/__test__/index.test.tsx
+++ src/components/Counter/__test__/index.test.tsx
@@ -1,28 +1,30 @@
 import { render, fireEvent } from '@testing-library/react'
 import Counter from '..'
 
-test('renders the counter with initial state', () => {
-  const { getByTestId } = render(<Counter />)
-  const countElement = getByTestId('count')
-  expect(countElement).toHaveTextContent('0')
-})
+describe('State Increment & Decrememt', () => {
+  test('renders the counter with initial state', () => {
+    const { getByTestId } = render(<Counter />)
+    const countElement = getByTestId('count')
+    expect(countElement).toHaveTextContent('0')
+  })
 
-test('increments the counter', () => {
-  const { getByTestId } = render(<Counter />)
-  const countElement = getByTestId('count')
-  const incrementButton = getByTestId('increment')
+  test('increments the counter', () => {
+    const { getByTestId } = render(<Counter />)
+    const countElement = getByTestId('count')
+    const incrementButton = getByTestId('increment')
 
-  fireEvent.click(incrementButton)
+    fireEvent.click(incrementButton)
 
-  expect(countElement).toHaveTextContent('1')
-})
+    expect(countElement).toHaveTextContent('1')
+  })
 
-test('decrements the counter', () => {
-  const { getByTestId } = render(<Counter />)
-  const countElement = getByTestId('count')
-  const decrementButton = getByTestId('decrement')
+  test('decrements the counter', () => {
+    const { getByTestId } = render(<Counter />)
+    const countElement = getByTestId('count')
+    const decrementButton = getByTestId('decrement')
 
-  fireEvent.click(decrementButton)
+    fireEvent.click(decrementButton)
 
-  expect(countElement).toHaveTextContent('-1')
+    expect(countElement).toHaveTextContent('-1')
+  })
 })
